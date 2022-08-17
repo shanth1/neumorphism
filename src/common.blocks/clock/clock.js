a952d163aa6fdb2aec39da3db28b0d4b28757a58
shanth1 <shanthi.bunch@gmail.com>
2022-08-18 01:35:15 +0300

    round-indicator

--- src/common.blocks/clock/clock.js
+++ src/common.blocks/clock/clock.js
@@ -7,11 +7,11 @@ const second = document.querySelector('.clock__second');
 // const transition = 'transition: 0.5s cubic-bezier(0.9, 0, 0, 1);'
 const transition = 'transition: 0.0s '
 
-let day = new Date();
+let date = new Date();
 
-let hrDeg = day.getHours() * 30
-let mnDeg = day.getHours() * 360 + day.getMinutes() * deg
-let scDeg = day.getMinutes() * 360 + day.getSeconds() * deg
+let hrDeg = date.getHours() * 30
+let mnDeg = date.getHours() * 360 + date.getMinutes() * deg
+let scDeg = date.getMinutes() * 360 + date.getSeconds() * deg
 
 hour.style.transform = `rotate(${(hrDeg)+(mnDeg/12)}deg)`;
 minute.style.transform = `rotate(${mnDeg}deg)`;
@@ -25,17 +25,20 @@ setInterval(() => {
     scDeg = day.getMinutes() * 360 + day.getSeconds() * deg
 
     
-    indicators[Math.round(day.getSeconds())].style.backgroundPosition = `50% 1%`;
+    indicators[Math.round(day.getSeconds())].style.height = `340px`;
+
     if (Math.round(day.getSeconds())===0){
-        indicators[59].style.backgroundPosition = `50% 2%`;
+        indicators[59].style.height = `335px`;
     }else{
-        indicators[Math.round(day.getSeconds())-1].style.backgroundPosition = `50% 2%`;
+        indicators[Math.round(day.getSeconds())-1].style.height = `335px`;
     }
     
     hour.style = `transform: rotate(${(hrDeg)+(mnDeg/12)}deg);  ${transition}`;
     minute.style = `transform: rotate(${mnDeg}deg); ${transition}`;
     second.style = `transform: rotate(${scDeg}deg);  ${transition}`;
 })
+
+console.log(date.getDate())
 
 let indicators = []
 for (let i = 0; i < 60; i++) {
@@ -45,13 +48,11 @@ for (let i = 0; i < 60; i++) {
     if (i%5===0){
         indicators[i].style.backgroundSize = `10px`
     }
+    indicators[i].style.height = '335px'
     indicators[i].style.rotate = `${i*6}deg`
-    console.log(Math.round(day.getMinutes() / 5)-1)
-    // if (Math.round(day.getMinutes() / 5)-1===i){
-    //     indicators[i].style.backgroundSize = `10px`
-    // }
-    document.querySelector('.clock').appendChild(indicators[i])
+    document.querySelector('.clock').append(indicators[i])
 }
+
 
 
 
