64f1ae1c12bd6d5b63ed566d180d831cd0222ef4
shanth1 <shanthi.bunch@gmail.com>
2022-08-19 16:24:06 +0300

    fixed webpack index.css

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
             template: './src/index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: 'index[hash].css'
+            filename: 'index.[hash].css'
         })
     ]
 };
